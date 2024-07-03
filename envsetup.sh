84d7cbd5864eb36553e5e527821deb1cdc0e0ad4
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-07-03 17:30:48 +0530

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -30,12 +30,12 @@ echo $PWD
 echo -e "\n\n\n"
 
 echo "Activating the virtual environment"
-source /var/lib/jenkins/worksapce/praveen/bin/activate
+source /var/lib/jenkins/worksapce/teerdha17/praveen/bin/activate
 echo -e "\n\n\n"
 
 echo -e "\n\n\n"
 echo "Installing Requirements...."
-pip3 install -r /var/lib/jenkins/workspace requirements.txt
+pip3 install -r /var/lib/jenkins/workspaceteerdha17 requirements.txt
 echo "Requirements Installed."
 echo -e "\n\n\n"
 
