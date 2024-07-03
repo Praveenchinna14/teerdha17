da0922cf0310539da4a8133077e061169e338878
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-07-03 14:44:41 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -4,13 +4,13 @@ cd /var/lib/jenkins/workspace/teerdha17
 
 sudo cp -rf nginx.conf /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/nginx.conf
-sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
+#sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
 chmod 710 /var/lib/jenkins/workspace/teerdha17
 
-sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
-sudo service nginx reload
-sudo service nginx start
+#sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
+
 sudo service nginx enable
+sudo service nginx start
 
 echo "Nginx has been started"
 
